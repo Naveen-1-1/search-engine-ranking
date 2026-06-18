3a66c0ee8294ffdaae5b1c0f01fcc0bb47b30bde
Naveen Shankar <naveenshankar2010@gmail.com>
2026-06-18 14:43:01 -0400

    basic routes data added

--- backend.js
+++ backend.js
@@ -1,5 +1,6 @@
 import express from "express";
 import testRouter from "./routes/test.js";
+import listingsRouter from "./routes/listings.js";
 
 console.log("Initializing the backend...");
 const PORT = process.env.PORT || 3000;
@@ -9,6 +10,8 @@ const app = express();
 app.use(express.static("frontend"));
 
 app.use("", testRouter);
+
+app.use("/api/", listingsRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
