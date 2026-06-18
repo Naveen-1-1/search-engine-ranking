e53a365201f9b47a98a110a3d8d8ff5fd790b032
Naveen Shankar <naveenshankar2010@gmail.com>
2026-06-17 22:55:23 -0400

    simple backend update done

---
+++ backend.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+console.log("Initializing the backend...");
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
