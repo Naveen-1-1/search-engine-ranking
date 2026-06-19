a6671ee3f945e48e6530bc64c06e723c5859af41
Naveen Shankar <naveenshankar2010@gmail.com>
2026-06-18 21:02:50 -0400

    packages script updated

--- add_packages.sh
+++ add_packages.sh
@@ -1,2 +1,2 @@
 npm install --save-dev eslint eslint-config-prettier prettier globals @eslint/js jest eslint-plugin-jest eslint-plugin-prettier live-server nodemon
-npm install express
+npm install express mongodb dotenv
